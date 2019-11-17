25e84f68b8534f29381d02d4e823f9cfa786ff6b
TheRealNOIG <tyler.gregorcyk@gmail.com>
2019-11-16 18:09:44 -0600

    Implemented OBJ Loader

--- GrzeEngine/Engine/Utils/ResourceManager.cs
+++ GrzeEngine/Engine/Utils/ResourceManager.cs
@@ -1,23 +1,50 @@
-using System.IO;
-using System.Reflection;
-using GrzeEngine.Engine.Logging;
-using GrzeEngine.Properties;
+using OpenGL;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using static OpenGL.GenericVAO;
 
 namespace GrzeEngine.Engine.Utils
 {
     public static class ResourceManager
     {
 
-        public static string getSourceFile(string path)
+        public static VAO LoadOBJModel(string obj, ShaderProgram shader)
         {
-            return "This function has been removed";
-            /*
-            using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(path))
-            using (StreamReader reader = new StreamReader(stream))
+            string[] lines = Regex.Split(obj, "\r\n|\r|\n");
+            List<Vector3> vertices = new List<Vector3>();
+            List<uint> indices = new List<uint>();
+
+            foreach (string line in lines)
             {
-                return reader.ReadToEnd();
+                string[] parts = line.Split(' ');
+                if (parts[0] == "v")
+                {
+                    vertices.Add(new Vector3(float.Parse(parts[1]), float.Parse(parts[2]), float.Parse(parts[3])));
+                }
+                else if (parts[0] == "f")
+                {
+                    string[] vertex1 = parts[1].Split('/');
+                    string[] vertex2 = parts[2].Split('/');
+                    string[] vertex3 = parts[3].Split('/');
+
+                    indices.AddRange(processVertex(vertex1));
+                    indices.AddRange(processVertex(vertex2));
+                    indices.AddRange(processVertex(vertex3));
+                }
             }
-            */
+            List<IGenericVBO> vbos = new List<IGenericVBO>();
+            vbos.Add(new GenericVBO<Vector3>(new VBO<Vector3>(vertices.ToArray()), "in_position"));
+            vbos.Add(new GenericVBO<uint>(new VBO<uint>(indices.ToArray(), BufferTarget.ElementArrayBuffer, BufferUsageHint.StaticRead)));
+            VAO model = new VAO(shader, vbos.ToArray());
+            return model;
+        }
+
+        private static List<uint> processVertex(string[] vertexData)
+        {
+            List<uint> indices = new List<uint>();
+            //TODO implement noramls and texture
+            indices.Add(uint.Parse(vertexData[0]) - 1);
+            return indices;
         }
     }
 }
