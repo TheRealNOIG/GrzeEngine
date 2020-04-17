2a60fc1860f166c1569b87e73b792886ab61d59e
Tyler <tyler.gregorcyk@gmail.com>
2020-04-16 21:43:25 -0700

    Added Event Handling in the Window class

--- GrzeEngine/Engine/Core/Window.cs
+++ GrzeEngine/Engine/Core/Window.cs
@@ -24,11 +24,6 @@ namespace GrzeEngine.Engine.Core
             Gl.Enable(EnableCap.DepthTest);
             Gl.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
 
-            //Setup InputHandling
-            for (int i = 8; i < 223; i++)
-                OpenGL.Platform.Input.Subscribe((char)i, new OpenGL.Platform.Event(HandleInput));
-            OpenGL.Platform.Input.Subscribe((char)27, OnExit);
-
             clock = new Clock();
 
             //setup logging system
@@ -39,7 +34,7 @@ namespace GrzeEngine.Engine.Core
         {
             while (OpenGL.Platform.Window.Open)
             {
-                OpenGL.Platform.Window.HandleEvents();
+                HandleEvents();
 
                 OnUpdate();
                 OnRender();
@@ -74,6 +69,59 @@ namespace GrzeEngine.Engine.Core
             OpenGL.Platform.Window.OnClose();
         }
 
-       
+        #region Event Handling
+        private SDL.SDL_Event sdlEvent;
+        private byte[] mouseState = new byte[256];
+
+        private void HandleEvents()
+        {
+            while (SDL.SDL_PollEvent(out sdlEvent) != 0 && OpenGL.Platform.Window.window != System.IntPtr.Zero)
+            {
+                switch (sdlEvent.type)
+                {
+                    case SDL.SDL_EventType.SDL_KEYDOWN:
+                        KeyboardManager.HandleInput((char)sdlEvent.key.keysym.sym, true);
+                        break;
+                    case SDL.SDL_EventType.SDL_KEYUP:
+                        KeyboardManager.HandleInput((char)sdlEvent.key.keysym.sym, false);
+                        break;
+                    case SDL.SDL_EventType.SDL_MOUSEBUTTONDOWN:
+                    case SDL.SDL_EventType.SDL_MOUSEBUTTONUP:
+                        // keep track of mouse state internally due to a bug in SDL
+                        // https://bugzilla.libsdl.org/show_bug.cgi?id=2195
+                        if (mouseState[sdlEvent.button.button] == sdlEvent.button.state) break;
+                        mouseState[sdlEvent.button.button] = sdlEvent.button.state;
+                        if (sdlEvent.button.y == 0 || sdlEvent.button.x == 0) mouseState[sdlEvent.button.button] = 0;
+
+                        OpenGL.Platform.Window.OnMouse(sdlEvent.button.button, sdlEvent.button.state, sdlEvent.button.x, sdlEvent.button.y);
+                        break;
+                    case SDL.SDL_EventType.SDL_MOUSEMOTION:
+                        OpenGL.Platform.Window.OnMovePassive(sdlEvent.motion.x, sdlEvent.motion.y);
+                        break;
+                    case SDL.SDL_EventType.SDL_MOUSEWHEEL:
+                        OpenGL.Platform.Window.OnMouseWheel?.Invoke(sdlEvent.wheel.which, sdlEvent.wheel.y, 0, 0);
+                        break;
+                    case SDL.SDL_EventType.SDL_WINDOWEVENT:
+                        switch (sdlEvent.window.windowEvent)
+                        {
+                            case SDL.SDL_WindowEventID.SDL_WINDOWEVENT_RESIZED:
+                                OpenGL.Platform.Window.OnReshape(sdlEvent.window.data1, sdlEvent.window.data2);
+                                break;
+                            case SDL.SDL_WindowEventID.SDL_WINDOWEVENT_CLOSE:
+                                OpenGL.Platform.Window.OnClose();
+                                break;
+                            case SDL.SDL_WindowEventID.SDL_WINDOWEVENT_MINIMIZED:
+                                // stop rendering the scene
+                                break;
+                            case SDL.SDL_WindowEventID.SDL_WINDOWEVENT_RESTORED:
+                            case SDL.SDL_WindowEventID.SDL_WINDOWEVENT_EXPOSED:
+                                // stop rendering the scene
+                                break;
+                        }
+                        break;
+                }
+            }
+        }
+        #endregion
     }
 }
